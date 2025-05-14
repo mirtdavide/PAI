c492b897b2c4747d8efc8d1735086918eb4d4541
valazzamatteo <matteo.valazza@student.tuiasi.ro>
2025-05-14 15:14:47 +0300

    htm to php3

---
+++ thread_form/thread_form.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="thread_form.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    </head>
+    <body>
+        <?php require 'header/header.php'; ?>
+        <div class="main-container">
+            <div class="thread-container">
+                <h2>Thread</h2>
+                <form>
+                    <input type="text" class="input-field" placeholder="Title" required>
+                    <select class="form-categori" required>
+                        <option value="option1">Author</option>
+                        <option value="option2">Novel</option>
+                        <option value="option3">Fan art</option>
+                        <option value="option4">Character</option>
+                        <option value="option5">Fan fiction</option>
+                        <option value="option6">News</option>
+                        <option value="option7">Adaptations</option>
+                        <option value="option8">Misc</option>
+                    </select> 
+                    <textarea class="description" rows="4" cols="50" placeholder="Description"></textarea>             
+                    <button type="submit" class="submit-button">Create</button>
+                </form>
+            </div>
+        </div>
+        <?php require 'footer/footer.php'; ?>     
+    </body>    
+</html>
