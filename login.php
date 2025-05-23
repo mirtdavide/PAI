231a4b656bad8929840d01727b7c66d2dac725b3
mirtdavide <mirtdavide@gmail.com>
2025-05-23 18:05:08 +0300

    Fixed login header

--- login.php
+++ login.php
@@ -1,42 +1,11 @@
 <html>
     <head>
-        <link rel="stylesheet" href="signup.css">
+        <link rel="stylesheet" href="login.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     </head>
     <body>
-        <div class = "header">
-            <div class = "nav">
-                <div class="logo">
-                    <img src="images/logo.png" alt="Logo">
-                    <p>Placeholder</p>
-                </div>
-                <a href = "threads_search.php" class = "nav_button">Threads</a>
-                <a href = "members.php" class = "nav_button">Members</a>
-                <div class = "search-container">
-                    <input type = "text" class = "search" placeholder = "Looking for a particular thread?">
-                    <div class="select-wrapper"></div>
-                        <select class="categori" required>
-                            <option value="option1">Misc</option>
-                            <option value="option2">Novel</option>
-                            <option value="option3">Fan art</option>
-                            <option value="option4">Character</option>
-                            <option value="option5">Fan fiction</option>
-                            <option value="option6">News</option>
-                            <option value="option7">Adaptations</option>
-                            <option value="option8">Author</option>
-                        </select> 
-                     </div>
-                <a href = "profile.php" class = "nav_button">My profile</a>
-                <a href = "thread_form.php" class = "nav_button">New thread</a>
-                <a href = "index.php" >Home</a>
-                <button class = "nav-button" onclick = "window.location.href = 'login.php'" >Login</button>
-                <button class = "nav-button" onclick = "window.location.href = 'signup.php'" >Signup</button>
-            </div>
-        </div>
-        <div>
-        <div class="titel">
-            <img src="images/logo_purple.png" alt="Logo">
-        </div class="main-container">
+        <?php require 'header.php'; ?>
+        <div class="main-container">
             <div class="signup-wrapper">
                 <div class="signup-image">
                     <img src="images/Bookshelf-Wallpaper-About-Murals.jpg" alt="Signup Image">
@@ -53,6 +22,6 @@
                 </div>
             </div>
         </div>
-                <?php require 'footer.php'; ?>      
+        <?php require 'footer.php'; ?>
     </body>    
 </html>
