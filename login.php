18c6829843a99142aa52773f0826af6a4437f2a3
mirtdavide <mirtdavide@gmail.com>
2025-05-23 18:36:05 +0300

    Changed login.php to require the header from header.php

--- login.php
+++ login.php
@@ -4,36 +4,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     </head>
     <body>
-        <div class = "header">
-            <div class = "nav">
-                <div class="logo">
-                    <img src="images/logo.png" alt="Logo">
-                    <p>Placeholder</p>
-                </div>
-                <a href = "threads_search.php" class = "nav_button">Threads</a>
-                <a href = "members.php" class = "nav_button">Members</a>
-                <div class = "search-container">
-                    <input type = "text" class = "search" placeholder = "Looking for a particular thread?">
-                    <div class="select-wrapper"></div>
-                        <select class="categori" required>
-                            <option value="option1">Misc</option>
-                            <option value="option2">Novel</option>
-                            <option value="option3">Fan art</option>
-                            <option value="option4">Character</option>
-                            <option value="option5">Fan fiction</option>
-                            <option value="option6">News</option>
-                            <option value="option7">Adaptations</option>
-                            <option value="option8">Author</option>
-                        </select> 
-                     </div>
-                <a href = "profile.php" class = "nav_button">My profile</a>
-                <a href = "thread_form.php" class = "nav_button">New thread</a>
-                <a href = "index.php" >Home</a>
-                <button class = "nav-button" onclick = "window.location.href = 'login.php'" >Login</button>
-                <button class = "nav-button" onclick = "window.location.href = 'signup.php'" >Signup</button>
-            </div>
-        </div>
-        <div>
+        <?php require 'header.php'; ?>
         <div class="titel">
             <img src="images/logo_purple.png" alt="Logo">
         </div class="main-container">
