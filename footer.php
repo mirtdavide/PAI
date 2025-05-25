248aea90d78e33898069c400d31ab467e30f4c22
mirtdavide <mirtdavide@gmail.com>
2025-05-26 01:17:54 +0300

    fixed icons on thread_search

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <head>
 <link rel="stylesheet" href="footer.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <footer>
     <div class="footer-container">
